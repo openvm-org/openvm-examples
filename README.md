bd07f6439aa2532e6b47535907fd898764065de4
luffykai <lunkai@intrinsictech.xyz>
2024-12-14 23:25:20 -0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# An example program using OpenVM
+
+## Running on host
+
+```bash
+printf '\xA0\x86\x01\x00\x00\x00\x00\x00' | cargo run --features std
+```
+## Running with OpenVM runtime
+
+TODO
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use openvm::io::read;
+
+fn main() {
+    let n: u64 = read();
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
+    for _ in 0..n {
+        let c: u64 = a.wrapping_add(b);
+        a = b;
+        b = c;
+    }
+    println!("{}", a);
+}
