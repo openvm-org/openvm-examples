20d8a6eb723dc252b23854695503dc281722799d
luffykai <lunkai@intrinsictech.xyz>
2024-12-15 15:24:25 -0800

    update script

--- make_input.py
+++ make_input.py
@@ -1,2 +1,3 @@
-with open('outputs/input_data.txt', 'wb') as f:    # Note the 'wb' mode for binary writing
-    f.write(b'\x0A\x00\x00\x00\x00\x00\x00\x00') 
+with open('outputs/input_data2.txt', 'wb') as f:    # Note the 'wb' mode for binary writing
+    n = 10
+    f.write(n.to_bytes(8, "little"))
